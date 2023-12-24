4f04051f998dd583587c9ca791b09be2d626293e
Max Feldman <mackx@protonmail.com>
2023-12-24 12:02:46 -0600

    add basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import logo from './logo.svg';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"  element={<Home/>} />
+        </Routes>
+    
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,70 @@
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Card from 'react-bootstrap/Card';
+import {Row, Col} from 'react-bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Home.scss';
+
+const inspLinks = [
+                    "https://images.nationalgeographic.org/image/upload/v1638892282/EducationHub/photos/isaac-newton-kneller-painting.jpg",
+                    "https://upload.wikimedia.org/wikipedia/commons/3/3a/Young_disraeli.jpg",
+                    "https://cdn.britannica.com/14/84514-050-AF43A0D9/Alan-M-Turing-1951.jpg",
+                    "https://www.laphamsquarterly.org/sites/default/files/styles/tall_rectangle_custom_user_small_2x/public/images/contributor/herzl.jpg?itok=G3G3cgDt&timestamp=1423765362",
+                    "https://manhattanrarebooks.cdn.bibliopolis.com/pictures/2187.jpg?auto=webp&v=1546625453",
+                    "https://dms-cf-09.dimu.org/image/022ynUn8ztQV?dimension=1200x1200"
+                ];
+
+const inspNames = ["Isaac Newton", "Benjamin D'Israeli", "Alan Turing", "Theodor Herzl", "Albert Einstein", "Haakon VII"];
+
+const inspDescs = [
+                    "Father of calculus, The man who discovered gravity",
+                    "Prime minister of the United Kingdom 1868,1874-1880",
+                    "Father of computer science",
+                    "Instrumental in the founding of the state of Israel",
+                    "Developed special and general relativity",
+                    "King of Norway, a man of great honour and valour"
+                ];
+
+function Home() {
+    let start = (new Date(2021, 10, 1)).getTime();
+    let graduation = (new Date(2025, 4, 10)).getTime(); //May 10th, 2025
+    let current = (new Date()).getTime();
+    console.log("HOME RENDER", graduation, current, start);
+    console.log(graduation - current, graduation - start);
+    let pct = Math.min(100, 100 * (current - start) / (graduation - start));
+
+    let inspCards = [];
+    for (let i = 0; i < inspLinks.length; i++) {
+        inspCards.push(
+            <Col className='d-flex'>
+                <Card style={{ width: '18rem' }} className='flex-fill'>
+                    <Card.Img variant="top" src={inspLinks[i]} />
+                    <Card.Body>
+                    <Card.Title>{inspNames[i]}</Card.Title>
+                    <Card.Text>{inspDescs[i]}</Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+        );
+    }
+    return (<>
+        <br />
+        <br />
+        <h1>About Me</h1>
+        <br />
+        <p className='homeCont'>
+            Hello internet, this is the official personal webpage of Max Feldman.
+            I am a Junior at the University of Texas at Austin persuing dual Bachelor of Science degrees in Computer Science and Mathematics.
+            I am also persuing a minor in the Hebrew language.
+        </p>
+        <h4>Degree Progress {pct.toFixed(3)}%</h4>
+        <ProgressBar now={pct} className='homeCont' />
+        <br />
+        <h3>Inspirations</h3>
+        <Row lg={inspCards.length} className='homeCont'>
+            {inspCards}
+        </Row>
+    </>);
+}
+
+export default Home;
