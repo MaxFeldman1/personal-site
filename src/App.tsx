8fe8f064288e2c3ee9c10759329c57d262bc5a59
Max Feldman <mackx@protonmail.com>
2023-12-27 02:29:59 -0600

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import logo from './logo.svg';
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 import Home from './Components/Home/Home';
 import AboutMe from './Components/AboutMe/AboutMe';
 import './App.css';
@@ -9,13 +10,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/"  element={<Home/>} />
-          <Route path="/AboutMe" element={<AboutMe />} />
-        </Routes>  
-      </BrowserRouter>
+      <Header />
+      <div className='main'>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/"  element={<Home/>} />
+            <Route path="/AboutMe" element={<AboutMe />} />
+          </Routes>  
+        </BrowserRouter>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.scss"
+
+function Footer() {
+    return (<>
+        <footer>
+            <p><a href="/">Max Feldman</a> © 2023</p>
+        </footer>
+    </>);
+}
+
+export default Footer;
